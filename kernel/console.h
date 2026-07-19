818b929587069004aafbab8d2d60ad3f43996c1d
Haruna Himekawa <haruna@ovsb.os>
2026-07-19 02:39:01 +0000

    feat(console): terminal VESA 80x45 ~ scrollback 200 linhas, serial mirror

---
+++ kernel/console.h
@@ -0,0 +1,14 @@
+#ifndef CONSOLE_H
+#define CONSOLE_H
+#include <stdint.h>
+
+void console_init(void);
+void console_putchar(char c);
+void console_write(const char *s);
+void console_printf(const char *fmt, ...);
+void console_clear(void);
+void console_show_cursor(int show);
+void console_set_fg(uint32_t color);
+void console_set_bg(uint32_t color);
+
+#endif
