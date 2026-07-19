da2da3fd0d29dadbcf141b129cba453a599e19d4
Haruna Himekawa <haruna@ovsb.os>
2026-07-19 02:39:01 +0000

    feat(serial): debug serial port ~ printf pros devs sofrerem ♥

---
+++ kernel/serial.h
@@ -0,0 +1,9 @@
+/* ♥ SERIAL_H ~ "Serial: debug sem monitor!"
+ * Dica: puts manda string, putc manda char~
+ * Baka, usa isso pra debuggar ao invés de print na tela! */
+#ifndef SERIAL_H
+#define SERIAL_H
+void serial_init(void);
+void serial_putc(char c);
+void serial_puts(const char *s);
+#endif
