aa17fa0b9fce2960d0aa75c22d0e699603377977
Haruna Himekawa <haruna@ovsb.os>
2026-07-19 02:39:01 +0000

    feat(timer): PIC + PIT 100Hz ~ tick tick tick, timer_ticks global

---
+++ kernel/pit.h
@@ -0,0 +1,11 @@
+/* ♥ PIT Header ~ timer_ticks é volatil pq o IRQ0 muda ele!
+ * Dica: sempre declare volatile pra variáveis de ISR~
+ * Senão o compilador vai otimizar e você vai ler 0 pra sempre~ */
+#ifndef PIT_H
+#define PIT_H
+#include <stdint.h>
+extern volatile uint64_t timer_ticks;
+void timer_tick_handler(void);
+void pit_init(void);
+void sleep_ms(uint64_t ms);
+#endif
