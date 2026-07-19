71eaf32af008bae36f3f6f355c0adfa80e80a8c5
Mesugaki Dev <mesugaki@ovsb.os>
2026-07-19 01:47:10 +0000

    feat(owt): header principal ~ inclui tudo q vc precisa, preguiçoso!

---
+++ src/owt/owt.h
@@ -0,0 +1,19 @@
+/* ♥ OWT - Ovsb Widget Toolkit ~ "O bagulho é lindo!"
+ * Dica: só incluir esse header e gg~ <3
+ * Procura um widget? Temos! Hihi, se não tiver, faz vc msm >_<
+ * Agr com WM integrado, flw fi ~ kyun! */
+#ifndef OWT_H
+#define OWT_H
+#include "owt_base.h"
+#include "owt_draw.h"
+#include "owt_theme.h"
+#include "owt_window.h"
+#include "owt_label.h"
+#include "owt_button.h"
+#include "owt_textbox.h"
+#include "owt_statusbar.h"
+#include "owt_listview.h"
+#include "owt_combobox.h"
+#include "owt_menu.h"
+#include "owt_dialog.h"
+#endif
