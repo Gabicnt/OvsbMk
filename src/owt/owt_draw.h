8bb1618381e3298d7cc461b4bbaa8730cf269188
Mesugaki Dev <mesugaki@ovsb.os>
2026-07-19 01:47:10 +0000

    feat(draw): primitivas de desenho ~ pixel a pixel, um quadrado por vez ♥

---
+++ src/owt/owt_draw.h
@@ -0,0 +1,12 @@
+/* ♥ OWT DRAW ~ Primitivas de desenho moe!
+ * Dica: pixel a pixel, um quadrado por vez~
+ * Se travar, é pq vc passou coordenada errada seu baka >_<
+ * Font 8x16 linda, q nem vc~ kyun! <3 */
+#ifndef OWT_DRAW_H
+#define OWT_DRAW_H
+#include <stdint.h>
+void owt_draw_pixel(int x, int y, uint32_t color);
+void owt_draw_rect(int x, int y, int w, int h, uint32_t color);
+void owt_draw_text(int x, int y, const char *text, uint32_t color);
+void owt_draw_char(int x, int y, char c, uint32_t color);
+#endif
